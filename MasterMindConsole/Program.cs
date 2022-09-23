02e8f37b2cb501e041148af770ffb210c53260bc
Márk Nikházy <mark.nikhazy@noffz.com>
2022-09-23 18:54:13 +0200

    Add project files.

---
+++ MasterMindConsole/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMindConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputersGame(1000);
+        }
+        private static void ComputersGame(int repetitionNumber)
+        {
+            List<int> results = new List<int>();
+            for (int i = 0; i < repetitionNumber; i++)
+            {
+                MasterMind assist = new MasterMind();
+
+                var solution = CreateRandomSolution();
+
+                int counter = 0;
+
+                while (true)
+                {
+                    counter++;
+                    var tipp = assist.BestTipp();
+
+                    tipp.ScoreIt(solution.Values);
+
+                    //If the winner is found
+                    if (tipp.Scores.Where(x => x == 2).Count() == 4)
+                    {
+                        break;
+                    }
+
+                    assist.DecreaseSolutions(tipp);
+                }
+                Console.WriteLine($"Solution found in {counter} tipps.");
+                results.Add(counter);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"******************************************************");
+            Console.WriteLine($"Average tipp number: {Math.Round(results.Average(),2)}");
+            Console.WriteLine($"Max tipp number: {results.Max()}");
+            Console.WriteLine($"Min tipp number: {results.Min()}");
+            Console.ReadLine();
+        }
+
+        private static SolutionModel CreateRandomSolution()
+        {
+            Random rnd = new Random();
+            int[] resultValues = new int[4];
+            for (int j = 0; j < resultValues.Length; j++)
+            {
+                while (true)
+                {
+                    int newValue = rnd.Next(1, 6);
+                    if (resultValues.Contains(newValue) == false)
+                    {
+                        resultValues[j] = newValue;
+                        break;
+                    }
+                }
+            }
+            SolutionModel result = new SolutionModel();
+            result.AddValues(resultValues);
+
+            return result;
+        }
+
+        private static void PlayersGame()
+        {
+            MasterMind assist = new MasterMind();
+
+            Random rnd = new Random();
+            int[] resultValues = new int[4];
+            for (int i = 0; i < resultValues.Length; i++)
+            {
+                while (true)
+                {
+                    int newValue = rnd.Next(1, 6);
+                    if (resultValues.Contains(newValue) == false)
+                    {
+                        resultValues[i] = newValue;
+                        break;
+                    }
+                }
+            }
+            SolutionModel result = new SolutionModel();
+            result.AddValues(resultValues);
+            Console.WriteLine("Kitaláltam egyet. Tippelhetsz:");
+
+            int counter = 0;
+            while (true)
+            {
+                counter++;
+                Console.WriteLine($"#{counter}. Tipped:");
+                Console.ForegroundColor = ConsoleColor.Green;
+                string line = Console.ReadLine();
+                Console.ResetColor();
+                if (line == "solution")
+                {
+                    Console.WriteLine($"*Megoldás*: {result.Values[0]},{result.Values[1]},{result.Values[2]},{result.Values[3]}");
+                    continue;
+                }
+                if (line == "hint")
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    foreach (var possibility in assist.Possiblities)
+                    {
+                        if (possibility.CanBeSolution)
+                        {
+                            Console.WriteLine($"*Lehetőség*: {possibility.Solution.Values[0]},{possibility.Solution.Values[1]},{possibility.Solution.Values[2]},{possibility.Solution.Values[3]}");
+                        }
+                    }
+                    Console.ResetColor();
+                    continue;
+                }
+                SolutionModel tipp = new SolutionModel();
+                if (tipp.AddValues(line))
+                {
+                    tipp.ScoreIt(result.Values);
+                    if (tipp.Scores.Where(x => x == 2).Count() == 4)
+                    {
+                        break;
+                    }
+                    assist.DecreaseSolutions(tipp);
+                    Console.WriteLine($"Válasz: {tipp.Scores[0]},{tipp.Scores[1]},{tipp.Scores[2]},{tipp.Scores[3]} (Lehetséges megoldások: {assist.Possiblities.Where(x => x.CanBeSolution).Count()}, Legjobb tipp: {assist.BestTipp().ValuesToString()})");
+                }
+                else
+                {
+                    Console.WriteLine("Nem érvényes formátum!");
+                }
+            }
+
+            Console.WriteLine($"Nyertél!!!!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MasterMindConsole/SolutionModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMindConsole
+{
+    class SolutionModel
+    {
+        public int[] Values { get; set; }
+        public int[] Scores { get; set; }
+        public SolutionModel()
+        {
+            Values = new int[4];
+            Scores = new int[4];
+        }
+        public bool AddValues(int[] values)
+        {
+            Values = values;
+            return true;
+        }
+        public string ValuesToString()
+        {
+            return $"{Values[0]},{Values[1]},{Values[2]},{Values[3]}";
+        }
+        public bool AddValues(string line)
+        {
+            try
+            {
+                List<int> numbers = new List<int>();
+                numbers.Add(Int32.Parse(line[0].ToString()));
+                numbers.Add(Int32.Parse(line[1].ToString()));
+                numbers.Add(Int32.Parse(line[2].ToString()));
+                numbers.Add(Int32.Parse(line[3].ToString()));
+
+                if(line.Length > 4)
+                {
+                    return false;
+                }
+
+                foreach (var item in numbers)
+                {
+                    if(numbers.Where(x=>x==item).Count() != 1)
+                    {
+                        return false;
+                    }
+                    else if(item > 6 || item < 1)
+                    {
+                        return false;
+                    }
+                }
+
+                Values = numbers.ToArray();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public void ScoreIt(int[] solutionValues)
+        {
+            for (int i = 0; i < solutionValues.Length; i++)
+            {
+                if(Values[i] == solutionValues[i])
+                {
+                    Scores[i] = 2;
+                }
+                else if (solutionValues.Contains(Values[i]))
+                {
+                    Scores[i] = 1;
+                }
+            }
+            Scores = Scores.ToList().OrderBy(x => x).ToArray();
+        }
+    }
+}
